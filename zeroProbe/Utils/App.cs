1752399af10c8b19b1ccd369141a894acf011797
kostya-zero <kostyantop8@gmail.com>
2022-07-28 12:22:15 +0300

    Fix codacy warnings

--- zeroProbe/Utils/App.cs
+++ zeroProbe/Utils/App.cs
@@ -2,8 +2,12 @@ namespace zeroProbe.Utils;
 
 public class App
 {
-    public static void End(int exitCode = 0)
+    public static void End()
     {
         Environment.Exit(0);
+    }
+    public static void End(int exitCode)
+    {
+        Environment.Exit(exitCode);
     }
 }
