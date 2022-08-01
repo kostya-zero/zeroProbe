219b043f86968578e04f14e3745fc313a5927805
kostya-zero <kostyantop8@gmail.com>
2022-08-01 19:26:19 +0300

    Refactor LexerObject.cs

--- zeroProbe/Models/LexerObject.cs
+++ zeroProbe/Models/LexerObject.cs
@@ -2,7 +2,14 @@ namespace zeroProbe.Models;
 
 public class LexerObject
 {
-    public string FunctionType = "";
-    public string Arguments = "";
-    public string FunctionName = "";
+    public string FunctionType { get; set; }
+    public string Arguments { get; set; }
+    public string FunctionName { get; set; }
+
+    public LexerObject()
+    {
+        FunctionName = "";
+        FunctionType = "";
+        Arguments = "";
+    }
 }
